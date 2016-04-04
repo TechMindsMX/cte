13cd6a708b64c5a438750bb77867826364aadcbb
says <says.rodriguez@gmail.com>
2016-04-04 16:32:29 -0500

    #2 se agregan los elementos de referencia de los nuevos changesets

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,3 +1,11 @@
 databaseChangeLog = {
     include file: 'adding-all-domains.groovy'
+    include file: 'adding_profile_to_user.groovy'
+    include file: 'adding_role_admin.groovy'
+    include file: 'adding_role_admin_iecce.groovy'
+    include file: 'adding_role_autorized.groovy'
+    include file: 'adding_role_integrado.groovy'
+    include file: 'adding_role_integrado_operador.groovy'
+    include file: 'adding_role_operador_iecce.groovy'
+
 }

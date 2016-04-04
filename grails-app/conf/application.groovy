aa545a428235ff527a8ca87a546fa3a4d7febe9e
says <says.rodriguez@gmail.com>
2016-04-04 16:26:45 -0500

    #2 se agregan los requestMap para los diversos controllers

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -8,16 +8,39 @@ grails.plugin.databasemigration.updateOnStartFileNames = ['changelog.groovy']
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.cte.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.cte.UserRole'
 grails.plugin.springsecurity.authority.className = 'com.cte.Role'
-grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-	'/':                ['permitAll'],
-	'/error':           ['permitAll'],
-	'/index':           ['permitAll'],
-	'/index.gsp':       ['permitAll'],
-	'/shutdown':        ['permitAll'],
-	'/assets/**':       ['permitAll'],
-	'/**/js/**':        ['permitAll'],
-	'/**/css/**':       ['permitAll'],
-	'/**/images/**':    ['permitAll'],
-	'/**/favicon.ico':  ['permitAll']
+grails.plugin.springsecurity.securityConfigType = "InterceptUrlMap"
+grails.plugin.springsecurity.successHandler.defaultTargetUrl = '/dashboard'
+grails.plugin.springsecurity.interceptUrlMap = [
+'/':                        ['permitAll'],
+'/error':                   ['permitAll'],
+'/index':                   ['permitAll'],
+'/index.gsp':               ['permitAll'],
+'/shutdown':                ['permitAll'],
+'/assets/**':               ['permitAll'],
+'/**/js/**':                ['permitAll'],
+'/**/css/**':               ['permitAll'],
+'/**/images/**':            ['permitAll'],
+'/**/favicon.ico':          ['permitAll'],
+'/login':                   ['permitAll'],
+'/login/**':                ['permitAll'],
+'/logout':                  ['IS_AUTHENTICATED_FULLY'],
+'/logout/**':               ['IS_AUTHENTICATED_FULLY'],
+'/user/**':                 ['permitAll'],
+'/confirmation/**':         ['permitAll'],
+'/recovery/**':             ['permitAll'],
+'/dashboard/**':            ['IS_AUTHENTICATED_REMEMBERED'],
+'/company/**':              ['IS_AUTHENTICATED_FULLY'],
+'/address/**':              ['IS_AUTHENTICATED_FULLY'],
+'/bankAccount/**':          ['IS_AUTHENTICATED_FULLY'],
+'/uploadDocuments/**':      ['IS_AUTHENTICATED_FULLY'],
+'/telephone/**':            ['IS_AUTHENTICATED_FULLY'],
+'/legalRepresentative/**':  ['IS_AUTHENTICATED_FULLY'],
+'/requestCompany/**':       ['IS_AUTHENTICATED_FULLY'],
+'/mamangerApplication/**':  ['IS_AUTHENTICATED_FULLY'],
+'/activation/**':           ['permitAll'],
+'/documentTemplates/**':    ['permitAll'],
+'/depositOrder/**':         ['IS_AUTHENTICATED_FULLY'],
+'/cashOutOrder/**':         ['IS_AUTHENTICATED_FULLY'],
+'/payroll/**':              ['IS_AUTHENTICATED_FULLY'],
+'/**':                      ['IS_AUTHENTICATED_REMEMBERED']
 ]
-

344e2414f5d29b61cc91934445bbff00246f0519
says <says.rodriguez@gmail.com>
2016-04-26 12:16:29 -0500

    #13 se agrega la propiedad company al object

--- grails-app/services/com/cte/ProcessorPayrollService.groovy
+++ grails-app/services/com/cte/ProcessorPayrollService.groovy
@@ -5,9 +5,10 @@ import grails.transaction.Transactional
 @Transactional
 class ProcessorPayrollService {
 
-    def createRelationIntoPayrolls(List payRolls,String nameFile) {
+    def createRelationIntoPayrolls(List payRolls,String nameFile,Company company) {
       def processorPayroll = new ProcessorPayroll()
       processorPayroll.nameFile = nameFile
+      processorPayroll.company = company
       payRolls.each { payroll ->
         processorPayroll.addToPayrolls(payroll)
       }

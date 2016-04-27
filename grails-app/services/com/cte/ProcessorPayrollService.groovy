26c6d4f15a9bf5676b0474ff97b9236bbf71e53d
says <says.rodriguez@gmail.com>
2016-04-27 10:43:59 -0500

    #14 se agrega los metodos para el envio de un correo al crear una nueva dispercion

--- grails-app/services/com/cte/ProcessorPayrollService.groovy
+++ grails-app/services/com/cte/ProcessorPayrollService.groovy
@@ -5,6 +5,9 @@ import grails.transaction.Transactional
 @Transactional
 class ProcessorPayrollService {
 
+  def restService
+  def grailsApplication
+
     def createRelationIntoPayrolls(List payRolls,String nameFile,Company company) {
       def processorPayroll = new ProcessorPayroll()
       processorPayroll.nameFile = nameFile
@@ -13,5 +16,20 @@ class ProcessorPayrollService {
         processorPayroll.addToPayrolls(payroll)
       }
       processorPayroll.save()
+      notificationToAdminTheNewProcessorPayRoll(processorPayroll)
+      processorPayroll
+    }
+
+    def notificationToAdminTheNewProcessorPayRoll(ProcessorPayroll processorPayroll) {
+      def message = createEmailNotification(processorPayroll,"la Empresa ${processorPayroll.company}, ha generado un proceso de dispercion de fondos")
+      restService.sendCommand(message,grailsApplication.config.emailer.notificationProcessorPayroll)
+    }
+
+    private def createEmailNotification(ProcessorPayroll processor, String message) {
+      def emailNotificationCommand = new EmailNotificationToIntegratedCommand()
+      emailNotificationCommand.emailResponse = grailsApplication.config.emailer.emailAdmin
+      emailNotificationCommand.message = message
+      emailNotificationCommand.url = grailsApplication.config.url.processorPayrolls
+      emailNotificationCommand
     }
 }
